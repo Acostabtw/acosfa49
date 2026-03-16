57aa7eab11cc76da024df94e867eb9c5b4092430
Acostabtw <sebastianacosta.grimont@gmail.com>
2026-03-15 22:22:37 -0400

    cost

---
+++ iniciar_servidor.bat
@@ -0,0 +1,7 @@
+@echo off
+title Servidor Backend AcosFa
+echo Iniciando el servidor de la base de datos...
+echo Manten esta ventana abierta mientras usas la pagina web.
+echo.
+node server.js
+pause
